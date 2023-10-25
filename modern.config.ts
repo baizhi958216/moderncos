ffd15a5465e67e30564aeb489a44c6d65a8dd7a1
baizhi958216 <1475289190@qq.com>
2023-10-26 00:47:58 +0800

    feat: 开启SSR
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,12 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 
-// https://modernjs.dev/en/configure/app/usage
 export default defineConfig<'rspack'>({
   runtime: {
     router: true,
+    state: true,
   },
-  plugins: [
-    appTools({
-      bundler: 'experimental-rspack',
-    }),
-  ],
+  server: {
+    ssr: true,
+  },
+  plugins: [appTools()],
 });
