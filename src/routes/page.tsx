ec879455560495363b51059ebfa7557f58371561
baizhi958216 <1475289190@qq.com>
2023-10-29 21:30:37 +0800

    feat: 添加 Loader
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,28 +1,18 @@
+import { useLoaderData } from '@modern-js/runtime/router';
 import { Helmet } from '@modern-js/runtime/head';
 import { List } from 'antd';
 import Item from '../components/Item';
-
-const getAvatar = (users: Array<{ name: string; email: string }>) =>
-  users.map(user => ({
-    ...user,
-    avatar: `https://avatars.dicebear.com/v2/identicon/${user.name}.svg`,
-  }));
-
-const mockData = getAvatar([
-  { name: 'Thomas', email: 'w.kccip@bllmfbgv.dm' },
-  { name: 'Chow', email: 'f.lfqljnlk@ywoefljhc.af' },
-  { name: 'Bradley', email: 'd.wfovsqyo@gpkcjwjgb.fr' },
-  { name: 'Davis', email: '"t.kqkoj@utlkwnpwk.nu' },
-]);
+import { LoaderData } from './page.data';
 
 function App() {
+  const { data } = useLoaderData() as LoaderData;
   return (
     <div className="container lg mx-auto">
       <Helmet>
         <title>All</title>
       </Helmet>
       <List
-        dataSource={mockData}
+        dataSource={data}
         renderItem={info => <Item key={info.name} info={info} />}
       />
     </div>
